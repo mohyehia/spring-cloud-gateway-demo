2da31628f47ba7430447e8aea99a88a605c4d8fe
mohyehia <mohammedyehia99@gmail.com>
2024-09-16 08:58:29 +0300

    Adding distributed tracing

--- api-gateway/src/main/java/com/moh/yehia/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/moh/yehia/api/gateway/ApiGatewayApplication.java
@@ -2,11 +2,14 @@ package com.moh.yehia.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
