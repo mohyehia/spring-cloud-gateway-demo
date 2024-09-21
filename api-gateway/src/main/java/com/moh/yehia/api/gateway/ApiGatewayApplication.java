8052cf534d202533cb2c2bdefed5576245fc5424
mohyehia <mohammedyehia99@gmail.com>
2024-09-21 17:48:54 +0300

    Adding `CircuitBreaker` pattern for the `api-gateway`

--- api-gateway/src/main/java/com/moh/yehia/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/moh/yehia/api/gateway/ApiGatewayApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -21,4 +26,15 @@ public class ApiGatewayApplication {
         return exchange -> Mono.just("ANONYMOUS");
     }
 
+}
+
+@RestController
+class FallbackController {
+    private static final Logger log = Logger.getLogger(FallbackController.class.getName());
+
+    @GetMapping("/books-fallback")
+    Flux<Void> findBooksFallback() {
+        log.info("FallbackController :: findBooksFallback :: Returning empty list of books");
+        return Flux.empty();
+    }
 }
